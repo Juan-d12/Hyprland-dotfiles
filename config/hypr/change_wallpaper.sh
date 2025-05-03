69d4690b7190046e71e1b2d3ffcf44e6cf3f46fb
diego <jdrivaschirre@hotmail.com>
2025-05-03 15:20:24 -0500

    Update waybar colors and style

--- config/hypr/change_wallpaper.sh
+++ config/hypr/change_wallpaper.sh
@@ -9,11 +9,11 @@ hyprpaper_config_file="$HOME/.config/hypr/hyprpaper.conf"
 # Get a random wallpaper that is not the current one
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
+# Update the color palette (requieres pywal16)
+wal -i $WALLPAPER
+
 # Update the config file with the new wallpaper path and apply the selected wallpaper
 echo "preload = $WALLPAPER" >  $hyprpaper_config_file
 echo "wallpaper = , $WALLPAPER" >>  $hyprpaper_config_file
-
-# Update the color palette (requieres pywal16)
-wal -i $WALLPAPER
 
 notify-send "Wallpaper changed"
