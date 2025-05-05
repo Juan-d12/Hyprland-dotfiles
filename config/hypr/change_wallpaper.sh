5a7586376c4f48ddc362f65cbb6d832b387ac04d
diego <jdrivaschirre@hotmail.com>
2025-05-05 14:22:40 -0500

    fix change wallpaper script

--- config/hypr/change_wallpaper.sh
+++ config/hypr/change_wallpaper.sh
@@ -9,11 +9,14 @@ hyprpaper_config_file="$HOME/.config/hypr/hyprpaper.conf"
 # Get a random wallpaper that is not the current one
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
-# Update the color palette (requieres pywal16)
-wal -i $WALLPAPER
+# Apply the selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
 
-# Update the config file with the new wallpaper path and apply the selected wallpaper
+# Update the config file with the new wallpaper path so the change is persistent
 echo "preload = $WALLPAPER" >  $hyprpaper_config_file
 echo "wallpaper = , $WALLPAPER" >>  $hyprpaper_config_file
+
+# Update the color palette (requieres pywal16)
+wal -i $WALLPAPER -n
 
 notify-send "Wallpaper changed"
