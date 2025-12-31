c909a2c1bede15847db3cf814e10cd1030b7384e
diego <jdrivaschirre@hotmail.com>
2025-12-31 00:10:46 -0500

    Update hyprpaper conf and script

--- config/hypr/change_wallpaper.sh
+++ config/hypr/change_wallpaper.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR="$HOME/Wallpaper/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 # Path to your hyprpaper configuration file
 hyprpaper_config_file="$HOME/.config/hypr/hyprpaper.conf"
 
-# Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+# Get a random wallpaper
+WALLPAPER_NAME=$(ls $WALLPAPER_DIR | shuf -n 1)
+WALLPAPER="~/Wallpaper/$WALLPAPER_NAME"
 
 # Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+hyprctl hyprpaper wallpaper " , $WALLPAPER"
 
 # Update the config file with the new wallpaper path so the change is persistent
-echo "preload = $WALLPAPER" >  $hyprpaper_config_file
-echo "wallpaper = , $WALLPAPER" >>  $hyprpaper_config_file
+echo "wallpaper {" >  $hyprpaper_config_file
+echo "    monitor =" >>  $hyprpaper_config_file
+echo "    path = $WALLPAPER" >>  $hyprpaper_config_file
+echo "    fit_mode = cover" >>  $hyprpaper_config_file
+echo "}" >>  $hyprpaper_config_file
+echo "splash = false" >>  $hyprpaper_config_file
 
 # Update the color palette (requieres pywal16)
-wal -i $WALLPAPER -n
+wal -i ~/Wallpaper/$WALLPAPER_NAME -n
 
 notify-send "Wallpaper changed"
